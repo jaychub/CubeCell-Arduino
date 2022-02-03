af2c2beb53d10c06d4bb349719f64e1051d1c18e
Jay C <jcherrabi@live.com>
2022-02-03 20:25:13 +0100

    added BoardGetBatteryLevel

--- cores/asr650x/board/board.h
+++ cores/asr650x/board/board.h
@@ -34,117 +34,127 @@
 #endif
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-/*!
- * Possible power sources
- */
-enum BoardPowerSources
-{
-    USB_POWER = 0,
-    BATTERY_POWER,
-};
-
-/*!
- * Board Version
- */
-typedef union BoardVersion_u
-{
-    struct BoardVersion_s
+    /*!
+     * Possible power sources
+     */
+    enum BoardPowerSources
     {
-        uint8_t Rfu;
-        uint8_t Revision;
-        uint8_t Minor;
-        uint8_t Major;
-    }Fields;
-    uint32_t Value;
-}BoardVersion_t;
+        USB_POWER = 0,
+        BATTERY_POWER,
+    };
 
-/*!
- * \brief Disable interrupts
- *
- * \remark IRQ nesting is managed
- */
-void BoardDisableIrq( void );
+    /*!
+     * Board Version
+     */
+    typedef union BoardVersion_u
+    {
+        struct BoardVersion_s
+        {
+            uint8_t Rfu;
+            uint8_t Revision;
+            uint8_t Minor;
+            uint8_t Major;
+        } Fields;
+        uint32_t Value;
+    } BoardVersion_t;
 
-/*!
- * \brief Enable interrupts
- *
- * \remark IRQ nesting is managed
- */
-void BoardEnableIrq( void );
+    /*!
+     * \brief Disable interrupts
+     *
+     * \remark IRQ nesting is managed
+     */
+    void BoardDisableIrq(void);
 
-/*!
- * \brief Initializes the mcu.
- */
-void boardInitMcu( void );
+    /*!
+     * \brief Enable interrupts
+     *
+     * \remark IRQ nesting is managed
+     */
+    void BoardEnableIrq(void);
 
-/*!
- * \brief Resets the mcu.
- */
-void BoardResetMcu( void );
+    /*!
+     * \brief Initializes the mcu.
+     */
+    void boardInitMcu(void);
 
-/*!
- * \brief Initializes the boards peripherals.
- */
-void BoardInitPeriph( void );
+    /*!
+     * \brief Resets the mcu.
+     */
+    void BoardResetMcu(void);
 
-/*!
- * \brief De-initializes the target board peripherals to decrease power
- *        consumption.
- */
-void BoardDeInitMcu( void );
+    /*!
+     * \brief Initializes the boards peripherals.
+     */
+    void BoardInitPeriph(void);
 
-/*!
- * \brief Gets the current potentiometer level value
- *
- * \retval value  Potentiometer level ( value in percent )
- */
-uint8_t BoardGetPotiLevel( void );
+    /*!
+     * \brief De-initializes the target board peripherals to decrease power
+     *        consumption.
+     */
+    void BoardDeInitMcu(void);
 
-/*!
- * \brief Measure the Battery voltage
- *
- * \retval value  battery voltage in volts
- */
-uint32_t BoardGetBatteryVoltage( void );
+    /*!
+     * \brief Gets the current potentiometer level value
+     *
+     * \retval value  Potentiometer level ( value in percent )
+     */
+    uint8_t BoardGetPotiLevel(void);
 
+    /*!
+     * \brief Measure the Battery voltage
+     *
+     * \retval value  battery voltage in volts
+     */
+    uint32_t BoardGetBatteryVoltage(void);
 
-/*!
- * Returns a pseudo random seed generated using the MCU Unique ID
- *
- * \retval seed Generated pseudo random seed
- */
-uint32_t BoardGetRandomSeed( void );
+    /*!
+     * \brief Get the current battery level
+     *
+     * \retval value  battery level [  0: USB,
+     *                                 1: Min level,
+     *                                 x: level
+     *                               254: fully charged,
+     *                               255: Error]
+     */
+    uint8_t BoardGetBatteryLevel(void);
 
-/*!
- * \brief Gets the board 64 bits unique ID
- *
- * \param [IN] id Pointer to an array that will contain the Unique ID
- */
-void BoardGetUniqueId( uint8_t *id );
+    /*!
+     * Returns a pseudo random seed generated using the MCU Unique ID
+     *
+     * \retval seed Generated pseudo random seed
+     */
+    uint32_t BoardGetRandomSeed(void);
 
-/*!
- * \brief Get the board power source
- *
- * \retval value  power source [0: USB_POWER, 1: BATTERY_POWER]
- */
-uint8_t GetBoardPowerSource( void );
+    /*!
+     * \brief Gets the board 64 bits unique ID
+     *
+     * \param [IN] id Pointer to an array that will contain the Unique ID
+     */
+    void BoardGetUniqueId(uint8_t *id);
 
-/*!
- * \brief Get the board version
- *
- * \retval value  Version
- */
-BoardVersion_t BoardGetVersion( void );
+    /*!
+     * \brief Get the board power source
+     *
+     * \retval value  power source [0: USB_POWER, 1: BATTERY_POWER]
+     */
+    uint8_t GetBoardPowerSource(void);
 
-extern int FLASH_update(uint32_t dst_addr, const void *data, uint32_t size);
-extern int FLASH_read_at(uint32_t address, uint8_t *pData, uint32_t len_bytes);
+    /*!
+     * \brief Get the board version
+     *
+     * \retval value  Version
+     */
+    BoardVersion_t BoardGetVersion(void);
+
+    extern int FLASH_update(uint32_t dst_addr, const void *data, uint32_t size);
+    extern int FLASH_read_at(uint32_t address, uint8_t *pData, uint32_t len_bytes);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif // __BOARD_H__
-
--- cores/asr650x/board/src/asr_board.c
+++ cores/asr650x/board/src/asr_board.c
@@ -33,9 +33,25 @@
 
 SX126x_t SX126x;
 
-#define         ID1                                 ( 0x1FF80050 )
-#define         ID2                                 ( 0x1FF80054 )
-#define         ID3                                 ( 0x1FF80064 )
+#define ID1 (0x1FF80050)
+#define ID2 (0x1FF80054)
+#define ID3 (0x1FF80064)
+
+/*!
+ * Battery thresholds
+ */
+#define BATTERY_TYPE 0       // 0= SAFT 3.6v or 1=Lithium 4.2V
+#define BAT_LEVEL_EMPTY 0x01 // 254
+#define BAT_LEVEL_FULL 0xFE  // 1
+#if (BATTERY_TYPE)
+#define BATTERY_MAX_LEVEL 3600      // mV 3.6 for saft LS17500 and 4200 for lithium
+#define BATTERY_MIN_LEVEL 2400      // mV 2.4 minimum for saft LS17500 and 3.4 minimum for lithium
+#define BATTERY_SHUTDOWN_LEVEL 2300 // mV 2.3 minimum for saft LS17500 and 3.3 minimum for lithium
+#else
+#define BATTERY_MAX_LEVEL 4100      // mV 3.6 for saft LS17500 and 4100 for lithium
+#define BATTERY_MIN_LEVEL 3400      // mV 2.4 minimum for saft LS17500 and 3.4 minimum for lithium
+#define BATTERY_SHUTDOWN_LEVEL 3300 // mV 2.3 minimum for saft LS17500 and 3.3 minimum for lithium
+#endif
 
 /*!
  * Antenna switch GPIO pins objects
@@ -53,293 +69,293 @@ uint8_t gPaOptSetting = 0;
 static uint32_t gBaudRate = STDIO_UART_BAUDRATE;
 char gChipId[17];
 
-void SX126xIoInit( void )
+void SX126xIoInit(void)
 {
-    GpioInit( &SX126x.Spi.Nss, RADIO_NSS, OUTPUT, PIN_PUSH_PULL, PIN_PULL_UP, 1 );
-    GpioInit( &SX126x.BUSY, RADIO_BUSY, INPUT, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
-    GpioInit( &SX126x.DIO1, RADIO_DIO_1, INPUT, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
+    GpioInit(&SX126x.Spi.Nss, RADIO_NSS, OUTPUT, PIN_PUSH_PULL, PIN_PULL_UP, 1);
+    GpioInit(&SX126x.BUSY, RADIO_BUSY, INPUT, PIN_PUSH_PULL, PIN_NO_PULL, 0);
+    GpioInit(&SX126x.DIO1, RADIO_DIO_1, INPUT, PIN_PUSH_PULL, PIN_NO_PULL, 0);
 }
 
-void SX126xIoIrqInit( DioIrqHandler dioIrq )
+void SX126xIoIrqInit(DioIrqHandler dioIrq)
 {
-    GpioSetInterrupt( &SX126x.DIO1, RISING, IRQ_HIGH_PRIORITY, dioIrq );
+    GpioSetInterrupt(&SX126x.DIO1, RISING, IRQ_HIGH_PRIORITY, dioIrq);
 }
 
-void SX126xIoDeInit( void )
+void SX126xIoDeInit(void)
 {
-    GpioInit( &SX126x.Spi.Nss, RADIO_NSS, ANALOG, PIN_PUSH_PULL, PIN_PULL_UP, 1 );
-    GpioInit( &SX126x.BUSY, RADIO_BUSY, ANALOG, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
-    GpioInit( &SX126x.DIO1, RADIO_DIO_1, ANALOG, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
+    GpioInit(&SX126x.Spi.Nss, RADIO_NSS, ANALOG, PIN_PUSH_PULL, PIN_PULL_UP, 1);
+    GpioInit(&SX126x.BUSY, RADIO_BUSY, ANALOG, PIN_PUSH_PULL, PIN_NO_PULL, 0);
+    GpioInit(&SX126x.DIO1, RADIO_DIO_1, ANALOG, PIN_PUSH_PULL, PIN_NO_PULL, 0);
 }
 
-uint32_t SX126xGetBoardTcxoWakeupTime( void )
+uint32_t SX126xGetBoardTcxoWakeupTime(void)
 {
     return BOARD_TCXO_WAKEUP_TIME;
 }
 
-void SX126xReset( void )
+void SX126xReset(void)
 {
-    pinMode(SX126x.Reset.pin,OUTPUT);
-    DelayMs( 20 );
-    GpioInit( &SX126x.Reset, RADIO_RESET, OUTPUT, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
-    DelayMs( 40 );
-    GpioInit( &SX126x.Reset, RADIO_RESET, OUTPUT, PIN_PUSH_PULL, PIN_NO_PULL, 1 ); // internal pull-up
-    DelayMs( 20 );
-    pinMode(SX126x.Reset.pin,ANALOG);
+    pinMode(SX126x.Reset.pin, OUTPUT);
+    DelayMs(20);
+    GpioInit(&SX126x.Reset, RADIO_RESET, OUTPUT, PIN_PUSH_PULL, PIN_NO_PULL, 0);
+    DelayMs(40);
+    GpioInit(&SX126x.Reset, RADIO_RESET, OUTPUT, PIN_PUSH_PULL, PIN_NO_PULL, 1); // internal pull-up
+    DelayMs(20);
+    pinMode(SX126x.Reset.pin, ANALOG);
 }
 
-void SX126xWaitOnBusy( void )
+void SX126xWaitOnBusy(void)
 {
-    while( GpioRead( &SX126x.BUSY ) == 1 );
+    while (GpioRead(&SX126x.BUSY) == 1)
+        ;
 }
 
-void SX126xWakeup( void )
+void SX126xWakeup(void)
 {
-    BoardDisableIrq( );
+    BoardDisableIrq();
 
-    GpioWrite( &SX126x.Spi.Nss, 0 );
-    SpiInOut( &SX126x.Spi, RADIO_GET_STATUS );
-    SpiInOut( &SX126x.Spi, 0x00 );
+    GpioWrite(&SX126x.Spi.Nss, 0);
+    SpiInOut(&SX126x.Spi, RADIO_GET_STATUS);
+    SpiInOut(&SX126x.Spi, 0x00);
 
-    GpioWrite( &SX126x.Spi.Nss, 1 );
-	
+    GpioWrite(&SX126x.Spi.Nss, 1);
+
     // Wait for chip to be ready.
-    SX126xWaitOnBusy( );
-    
-    BoardEnableIrq( );
+    SX126xWaitOnBusy();
+
+    BoardEnableIrq();
 }
 
-void SX126xWriteCommand( RadioCommands_t command, uint8_t *buffer, uint16_t size )
+void SX126xWriteCommand(RadioCommands_t command, uint8_t *buffer, uint16_t size)
 {
 
-    SX126xCheckDeviceReady( );
-    GpioWrite( &SX126x.Spi.Nss, 0 );
+    SX126xCheckDeviceReady();
+    GpioWrite(&SX126x.Spi.Nss, 0);
 
-    SpiInOut( &SX126x.Spi, ( uint8_t )command );
+    SpiInOut(&SX126x.Spi, (uint8_t)command);
 
-    for( uint16_t i = 0; i < size; i++ )
+    for (uint16_t i = 0; i < size; i++)
     {
-        SpiInOut( &SX126x.Spi, buffer[i] );
+        SpiInOut(&SX126x.Spi, buffer[i]);
     }
 
-    GpioWrite( &SX126x.Spi.Nss, 1 );
+    GpioWrite(&SX126x.Spi.Nss, 1);
 
-    if( command != RADIO_SET_SLEEP )
+    if (command != RADIO_SET_SLEEP)
     {
-        SX126xWaitOnBusy( );
+        SX126xWaitOnBusy();
     }
 }
 
-void SX126xReadCommand( RadioCommands_t command, uint8_t *buffer, uint16_t size )
+void SX126xReadCommand(RadioCommands_t command, uint8_t *buffer, uint16_t size)
 {
 
-    SX126xCheckDeviceReady( );
-    GpioWrite( &SX126x.Spi.Nss, 0 );
+    SX126xCheckDeviceReady();
+    GpioWrite(&SX126x.Spi.Nss, 0);
 
-    SpiInOut( &SX126x.Spi, ( uint8_t )command );
-    SpiInOut( &SX126x.Spi, 0x00 );
-    for( uint16_t i = 0; i < size; i++ )
+    SpiInOut(&SX126x.Spi, (uint8_t)command);
+    SpiInOut(&SX126x.Spi, 0x00);
+    for (uint16_t i = 0; i < size; i++)
     {
-        buffer[i] = SpiInOut( &SX126x.Spi, 0 );
+        buffer[i] = SpiInOut(&SX126x.Spi, 0);
     }
 
-    GpioWrite( &SX126x.Spi.Nss, 1 );
+    GpioWrite(&SX126x.Spi.Nss, 1);
 
-    SX126xWaitOnBusy( );
+    SX126xWaitOnBusy();
 }
 
-void SX126xWriteRegisters( uint16_t address, uint8_t *buffer, uint16_t size )
+void SX126xWriteRegisters(uint16_t address, uint8_t *buffer, uint16_t size)
 {
-    SX126xCheckDeviceReady( );
+    SX126xCheckDeviceReady();
 
-    GpioWrite( &SX126x.Spi.Nss, 0 );
+    GpioWrite(&SX126x.Spi.Nss, 0);
 
-    SpiInOut( &SX126x.Spi, RADIO_WRITE_REGISTER );
-    SpiInOut( &SX126x.Spi, ( address & 0xFF00 ) >> 8 );
-    SpiInOut( &SX126x.Spi, address & 0x00FF );
-    
-    for( uint16_t i = 0; i < size; i++ )
+    SpiInOut(&SX126x.Spi, RADIO_WRITE_REGISTER);
+    SpiInOut(&SX126x.Spi, (address & 0xFF00) >> 8);
+    SpiInOut(&SX126x.Spi, address & 0x00FF);
+
+    for (uint16_t i = 0; i < size; i++)
     {
-        SpiInOut( &SX126x.Spi, buffer[i] );
+        SpiInOut(&SX126x.Spi, buffer[i]);
     }
-	
-    GpioWrite( &SX126x.Spi.Nss, 1 );
-	
-    SX126xWaitOnBusy( );
+
+    GpioWrite(&SX126x.Spi.Nss, 1);
+
+    SX126xWaitOnBusy();
 }
 
-void SX126xWriteRegister( uint16_t address, uint8_t value )
+void SX126xWriteRegister(uint16_t address, uint8_t value)
 {
-    SX126xWriteRegisters( address, &value, 1 );
+    SX126xWriteRegisters(address, &value, 1);
 }
 
-void SX126xReadRegisters( uint16_t address, uint8_t *buffer, uint16_t size )
+void SX126xReadRegisters(uint16_t address, uint8_t *buffer, uint16_t size)
 {
-    SX126xCheckDeviceReady( );
+    SX126xCheckDeviceReady();
 
-    GpioWrite( &SX126x.Spi.Nss, 0 );
+    GpioWrite(&SX126x.Spi.Nss, 0);
 
-    SpiInOut( &SX126x.Spi, RADIO_READ_REGISTER );
-    SpiInOut( &SX126x.Spi, ( address & 0xFF00 ) >> 8 );
-    SpiInOut( &SX126x.Spi, address & 0x00FF );
-    SpiInOut( &SX126x.Spi, 0 );
-    for( uint16_t i = 0; i < size; i++ )
+    SpiInOut(&SX126x.Spi, RADIO_READ_REGISTER);
+    SpiInOut(&SX126x.Spi, (address & 0xFF00) >> 8);
+    SpiInOut(&SX126x.Spi, address & 0x00FF);
+    SpiInOut(&SX126x.Spi, 0);
+    for (uint16_t i = 0; i < size; i++)
     {
-        buffer[i] = SpiInOut( &SX126x.Spi, 0 );
+        buffer[i] = SpiInOut(&SX126x.Spi, 0);
     }
-    GpioWrite( &SX126x.Spi.Nss, 1 );
+    GpioWrite(&SX126x.Spi.Nss, 1);
 
-    SX126xWaitOnBusy( );
-   
+    SX126xWaitOnBusy();
 }
 
-uint8_t SX126xReadRegister( uint16_t address )
+uint8_t SX126xReadRegister(uint16_t address)
 {
     uint8_t data;
-    SX126xReadRegisters( address, &data, 1 );
+    SX126xReadRegisters(address, &data, 1);
     return data;
 }
 
-void SX126xWriteBuffer( uint8_t offset, uint8_t *buffer, uint8_t size )
+void SX126xWriteBuffer(uint8_t offset, uint8_t *buffer, uint8_t size)
 {
-    
-    SX126xCheckDeviceReady( );
 
-    GpioWrite( &SX126x.Spi.Nss, 0 );
+    SX126xCheckDeviceReady();
 
-    SpiInOut( &SX126x.Spi, RADIO_WRITE_BUFFER );
-    SpiInOut( &SX126x.Spi, offset );
-    for( uint16_t i = 0; i < size; i++ )
+    GpioWrite(&SX126x.Spi.Nss, 0);
+
+    SpiInOut(&SX126x.Spi, RADIO_WRITE_BUFFER);
+    SpiInOut(&SX126x.Spi, offset);
+    for (uint16_t i = 0; i < size; i++)
     {
-        SpiInOut( &SX126x.Spi, buffer[i] );
+        SpiInOut(&SX126x.Spi, buffer[i]);
     }
-    GpioWrite( &SX126x.Spi.Nss, 1 );
+    GpioWrite(&SX126x.Spi.Nss, 1);
 
-    SX126xWaitOnBusy( );
+    SX126xWaitOnBusy();
 }
 
-void SX126xReadBuffer( uint8_t offset, uint8_t *buffer, uint8_t size )
+void SX126xReadBuffer(uint8_t offset, uint8_t *buffer, uint8_t size)
 {
-    SX126xCheckDeviceReady( );
+    SX126xCheckDeviceReady();
 
-    GpioWrite( &SX126x.Spi.Nss, 0 );
+    GpioWrite(&SX126x.Spi.Nss, 0);
 
-    SpiInOut( &SX126x.Spi, RADIO_READ_BUFFER );
-    SpiInOut( &SX126x.Spi, offset );
-    SpiInOut( &SX126x.Spi, 0 );
-    for( uint16_t i = 0; i < size; i++ )
+    SpiInOut(&SX126x.Spi, RADIO_READ_BUFFER);
+    SpiInOut(&SX126x.Spi, offset);
+    SpiInOut(&SX126x.Spi, 0);
+    for (uint16_t i = 0; i < size; i++)
     {
-        buffer[i] = SpiInOut( &SX126x.Spi, 0 );
+        buffer[i] = SpiInOut(&SX126x.Spi, 0);
     }
-    GpioWrite( &SX126x.Spi.Nss, 1 );
+    GpioWrite(&SX126x.Spi.Nss, 1);
 
-    SX126xWaitOnBusy( );
-   
+    SX126xWaitOnBusy();
 }
 
-void SX126xSetRfTxPower( int8_t power )
+void SX126xSetRfTxPower(int8_t power)
 {
-    SX126xSetTxParams( power, RADIO_RAMP_200_US );
+    SX126xSetTxParams(power, RADIO_RAMP_200_US);
 }
 
-uint8_t SX126xGetPaSelect( uint32_t channel )
+uint8_t SX126xGetPaSelect(uint32_t channel)
 {
     (void)channel;
     return SX1262;
 }
 
-uint8_t SX126xGetPaOpt( )
+uint8_t SX126xGetPaOpt()
 {
     return gPaOptSetting;
 }
 
-void SX126xSetPaOpt( uint8_t opt )
+void SX126xSetPaOpt(uint8_t opt)
 {
-    if(opt>3) return;
-    
+    if (opt > 3)
+        return;
+
     gPaOptSetting = opt;
 }
 
-void SX126xAntSwOn( void )
+void SX126xAntSwOn(void)
 {
-    //GpioInit( &AntPow, RADIO_ANT_SWITCH_POWER, OUTPUT, PIN_PUSH_PULL, PIN_PULL_UP, 1 );
+    // GpioInit( &AntPow, RADIO_ANT_SWITCH_POWER, OUTPUT, PIN_PUSH_PULL, PIN_PULL_UP, 1 );
 }
 
-void SX126xAntSwOff( void )
+void SX126xAntSwOff(void)
 {
-    //GpioInit( &AntPow, RADIO_ANT_SWITCH_POWER, ANALOG, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
+    // GpioInit( &AntPow, RADIO_ANT_SWITCH_POWER, ANALOG, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
 }
 
-bool SX126xCheckRfFrequency( uint32_t frequency )
+bool SX126xCheckRfFrequency(uint32_t frequency)
 {
     (void)frequency;
     // Implement check. Currently all frequencies are supported
     return true;
 }
 
-
-void BoardDisableIrq( void )
+void BoardDisableIrq(void)
 {
-    CyGlobalIntDisable;    
+    CyGlobalIntDisable;
 }
 
-void BoardEnableIrq( void )
+void BoardEnableIrq(void)
 {
     CyGlobalIntEnable;
 }
 
-void DelayMsMcu( uint32_t ms )
+void DelayMsMcu(uint32_t ms)
 {
     CyDelay(ms);
 }
 
 static char *olds = NULL;
-extern void *rawmemchr (__const void *__s, int __c);
-char * strtok_l (char *s, const char *delim)
+extern void *rawmemchr(__const void *__s, int __c);
+char *strtok_l(char *s, const char *delim)
 {
     char *token;
 
-    if (s == NULL) s = olds;
+    if (s == NULL)
+        s = olds;
 
-    s += strspn (s, delim);
-    if (*s == '\0') {
+    s += strspn(s, delim);
+    if (*s == '\0')
+    {
         olds = s;
         return NULL;
     }
 
     token = s;
-    s = strpbrk (token, delim);  
+    s = strpbrk(token, delim);
     if (s == NULL)
-        olds = rawmemchr (token, '\0');
-    else {      
-        *s = '\0';        
+        olds = rawmemchr(token, '\0');
+    else
+    {
+        *s = '\0';
         olds = s + 1;
     }
     return token;
 }
 
-
 static const double huge = 1.0e300;
-#define __HI(x) *(1+(int*)&x)
-#define __LO(x) *(int*)&x
+#define __HI(x) *(1 + (int *)&x)
+#define __LO(x) *(int *)&x
 
-static const double TWO52[2]={
-  4.50359962737049600000e+15, /* 0x43300000, 0x00000000 */
- -4.50359962737049600000e+15, /* 0xC3300000, 0x00000000 */
+static const double TWO52[2] = {
+    4.50359962737049600000e+15,  /* 0x43300000, 0x00000000 */
+    -4.50359962737049600000e+15, /* 0xC3300000, 0x00000000 */
 };
 
 extern uint32_t systime;
 
-
-void boardInitMcu( void )
+void boardInitMcu(void)
 {
     SpiInit();
     Asr_Timer_Init();
     RtcInit();
     systime = millis();
-#if defined(CubeCell_Board)||defined(CubeCell_Capsule)||defined(CubeCell_BoardPlus)||defined(CubeCell_GPS)||defined(CubeCell_HalfAA)
-    pinMode(Vext,OUTPUT);
-    digitalWrite(Vext,HIGH);
+#if defined(CubeCell_Board) || defined(CubeCell_Capsule) || defined(CubeCell_BoardPlus) || defined(CubeCell_GPS) || defined(CubeCell_HalfAA)
+    pinMode(Vext, OUTPUT);
+    digitalWrite(Vext, HIGH);
 
     /*
      * Board, BoardPlus, Capsule, GPS and HalfAA variants
@@ -372,7 +388,7 @@ char *HW_Get_MFT_ID(void)
 
 char *HW_Get_MFT_Model(void)
 {
-    return CONFIG_DEVICE_MODEL;   
+    return CONFIG_DEVICE_MODEL;
 }
 
 char *HW_Get_MFT_Rev(void)
@@ -389,12 +405,12 @@ char *HW_Get_MFT_SN(void)
 }
 
 bool HW_Set_MFT_Baud(uint32_t baud)
-{   
-    uint32_t div = (float)CYDEV_BCLK__HFCLK__HZ/baud/UART_1_UART_OVS_FACTOR + 0.5 - 1;
-    UART_1_SCBCLK_DIV_REG = div<<8;
+{
+    uint32_t div = (float)CYDEV_BCLK__HFCLK__HZ / baud / UART_1_UART_OVS_FACTOR + 0.5 - 1;
+    UART_1_SCBCLK_DIV_REG = div << 8;
     UART_1_SCBCLK_CMD_REG = 0x8000FF41u;
-    
-    gBaudRate = baud;   
+
+    gBaudRate = baud;
     return true;
 }
 
@@ -405,17 +421,85 @@ uint32_t HW_Get_MFT_Baud(void)
 
 void HW_Reset(int mode)
 {
-    if (mode == 0) {
-	    CySoftwareReset();
-    } else if (mode == 1) {
+    if (mode == 0)
+    {
+        CySoftwareReset();
+    }
+    else if (mode == 1)
+    {
         Bootloadable_1_Load();
-    } 
+    }
 }
 
-uint32_t BoardGetRandomSeed( void )
+uint32_t BoardGetRandomSeed(void)
 {
-    return ( ( *( uint32_t* )ID1 ) ^ ( *( uint32_t* )ID2 ) ^ ( *( uint32_t* )ID3 ) );
+    return ((*(uint32_t *)ID1) ^ (*(uint32_t *)ID2) ^ (*(uint32_t *)ID3));
 }
 
+/*  get the BatteryVoltage in mV. */
+uint32_t BoardGetBatteryVoltage(void)
+{
+    float temp = 0;
+    uint16_t volt;
+    uint8_t pin;
+    pin = ADC;
 
+#if defined(CubeCell_Board) || defined(CubeCell_Capsule) || defined(CubeCell_BoardPlus) || defined(CubeCell_BoardPRO) || defined(CubeCell_GPS) || defined(CubeCell_HalfAA)
+    /*
+     * have external 10K VDD pullup resistor
+     * connected to VBAT_ADC_CTL pin
+     */
 
+    pinMode(VBAT_ADC_CTL, OUTPUT);
+    digitalWrite(VBAT_ADC_CTL, LOW);
+#endif
+    for (int i = 0; i < 50; i++) // read 50 times and get average
+        temp += analogReadmV(pin);
+    volt = temp / 50;
+
+#if defined(CubeCell_Board) || defined(CubeCell_Capsule) || defined(CubeCell_BoardPlus) || defined(CubeCell_BoardPRO) || defined(CubeCell_GPS) || defined(CubeCell_HalfAA)
+    pinMode(VBAT_ADC_CTL, INPUT);
+#endif
+
+    volt = volt * 2;
+    return volt;
+}
+uint8_t BoardGetBatteryLevel(void)
+{
+    // 5.5 End-Device Status (DevStatusReq, DevStatusAns)
+    // 0      The end-device is connected to an external power source.
+    // 1..254 The battery level, 1 being at minimum and 254 being at maximum
+    // 255    The end-device was not able to measure the battery level.
+    const uint32_t batteryVoltage = BoardGetBatteryVoltage();
+    printf("The end-device BoardGetBatteryVoltage():%d \r\n", batteryVoltage);
+    uint8_t batteryLevel = 0;
+#if (BATTERY_TYPE)
+    const double maxBattery = 3.6; // 3.6 for saft LS17500 ;//4.2 for lithium; //must update to match SAFT 3.6 max voltage
+    const double minBattery = 2.4; // 2.4 minimum for saft LS17500 and 3.4 minimum for lithium
+#else
+    const double maxBattery = 4.1; // 3.6 for saft LS17500 ;//4.2 for lithium; //must update to match SAFT 3.6 max voltage
+    const double minBattery = 3.4; // 2.4 minimum for saft LS17500 and 3.4 minimum for lithium
+#endif
+    const double batVoltage = fmax(minBattery, fmin(maxBattery, batteryVoltage / 1000.0));
+
+    printf("The end-device BoardGetBatteryVoltage():%d batVoltage:%d\r\n", batteryVoltage, batVoltage);
+
+    if (batteryVoltage >= BATTERY_MAX_LEVEL)
+    {
+        batteryLevel = 0;
+    }
+    else if ((batteryVoltage > BATTERY_MIN_LEVEL) && (batteryVoltage < BATTERY_MAX_LEVEL))
+    {
+        batteryLevel = BAT_LEVEL_EMPTY + ((batVoltage - minBattery) / (maxBattery - minBattery)) * (BAT_LEVEL_FULL - BAT_LEVEL_EMPTY);
+    }
+    else if ((batteryVoltage > BATTERY_SHUTDOWN_LEVEL) && (batteryVoltage <= BATTERY_MIN_LEVEL) || (batteryVoltage <= BATTERY_SHUTDOWN_LEVEL))
+    {
+        batteryLevel = 1;
+    }
+    else
+    {
+        batteryLevel = 255;
+    }
+    // printf("battery level (1-254): %u\n", batlevel);
+    return batteryLevel;
+}
